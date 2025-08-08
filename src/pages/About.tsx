6e2fa4d9400fa49f50b6f445168633a006aad0d6
Pancham Sardar <sardarpancham510510@gmail.com>
2025-08-08 17:15:52 +0530

    chnages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -73,7 +73,7 @@ const About = () => {
   ];
 
   return (
-    <div className="min-h-screen pt-16">
+    <div className="min-h-screen">
       {/* Hero Section */}
       <section className="py-20 bg-gradient-accent">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
